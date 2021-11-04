6fd8f6eab787df25aa3256fe06cf4b698452929f
D9511pellegrino <52115987+D9511pellegrino@users.noreply.github.com>
2021-11-03 22:16:26 -0300

    Alguna cosa funcionando
    Implementado: abb_crear, abb_insertar, abb_vacia, abb_tamanio, abb_destuir, abb_destruir_todo.
    Chequeada:  abb_crear, abb_vacia, abb_tamanio,

--- pruebas.c
+++ pruebas.c
@@ -3,8 +3,42 @@
 #include "pa2mm.h"
 #include "string.h"
 
+int comparador_int(int* primero, int* segundo){
+  return *primero-*segundo;
+}
+
+void puedoCrearYDestruirUnABB(){
+  abb_t* a = NULL;
+
+  pa2m_afirmar((a=abb_crear((abb_comparador) comparador_int)), "Crear un ABB devuelve un ABB");
+  pa2m_afirmar(abb_vacio(a), "ABB se crea vacio")
+  pa2m_afirmar(abb_tamanio(a)==0, "ABB tiene 0 elementos");
+
+  abb_destruir(a);
+}
+
+
+void dadaUnABBVacio_Puedorealizaroperaciones(){
+  abb_t* a = abb_crear((abb_comparador) comparador_int);
+
+  int numeros[5] = {0,1,2,3,4};
+
+  pa2m_afirmar(abb_vacio(a), "ABB se crea vacio")
+  pa2m_afirmar(abb_tamanio(a)==0, "ABB tiene 0 elementos");
+
+  pa2m_afirmar(abb_insertar(a, &(numeros[0]))==a, "Puedo insertar un elemento en un ABB vacío");
+  abb_destruir(a);
+
+}
+
 int main(){
   pa2m_nuevo_grupo("Pruebas de ABB");
+
+  pa2m_nuevo_grupo("Pruebas de creacion y destruccion");
+  puedoCrearYDestruirUnABB();
+
+  pa2m_nuevo_grupo("Con un ABB vacío puedo operar");
+  dadaUnABBVacio_Puedorealizaroperaciones();
 
   return pa2m_mostrar_reporte();
 }
--- src/abb.c
+++ src/abb.c
@@ -3,35 +3,134 @@
 #include <stdlib.h>
 
 abb_t* abb_crear(abb_comparador comparador){
-    return NULL;
+    if(!comparador) return NULL;
+
+    abb_t* nuevo_abb = calloc(1,sizeof(abb_t));
+    if(!nuevo_abb) return NULL;
+
+    nuevo_abb->nodo_raiz = NULL;
+    nuevo_abb->comparador = comparador;
+    nuevo_abb->tamanio = 0;
+
+    return nuevo_abb;
+}
+
+nodo_abb_t* abb_insertar_recursivo(nodo_abb_t* nodo_padre, nodo_abb_t* nuevo_nodo, abb_comparador comparador){
+    if(!nodo_padre || !nuevo_nodo || !comparador) return NULL;
+
+    int comparacion = comparador(nuevo_nodo->elemento, nodo_padre->elemento);
+
+    if(comparacion<=0){
+        if(!nodo_padre->izquierda){
+            nodo_padre->izquierda = nuevo_nodo;
+            return nodo_padre->izquierda;
+        }
+        else return abb_insertar_recursivo(nodo_padre->izquierda, nuevo_nodo, comparador);
+    }
+    else if(comparacion>0){
+        if(!nodo_padre->derecha){
+          nodo_padre->derecha = nuevo_nodo;
+          return nodo_padre->derecha;
+        }
+        else return abb_insertar_recursivo(nodo_padre->derecha, nuevo_nodo, comparador);
+    }
+
+    return NULL; 
+
 }
 
 abb_t* abb_insertar(abb_t* arbol, void* elemento){
-  return arbol;
+    if(!arbol) return NULL;
+
+    nodo_abb_t* nuevo_nodo = calloc(1,sizeof(nodo_abb_t));
+    if(!nuevo_nodo) return NULL;
+
+    nuevo_nodo->elemento = elemento;
+    nuevo_nodo->izquierda = NULL;
+    nuevo_nodo->derecha = NULL;
+
+    if(arbol->tamanio == 0){
+        arbol->nodo_raiz = nuevo_nodo;
+    }
+    else arbol->nodo_raiz = abb_insertar_recursivo(arbol->nodo_raiz, nuevo_nodo, arbol->comparador);
+    if(!arbol->nodo_raiz) return NULL;
+
+    arbol->tamanio++;
+    return arbol;
 }
 
+
 void* abb_quitar(abb_t* arbol, void *elemento){
-  return NULL;
+    if(!arbol) return NULL;
+
+
+
+
+}
+
+void* abb_buscar_recursivo(nodo_abb_t* nodo_padre, void* elemento, abb_comparador comparador){
+  if(!nodo_padre) return NULL;
+
+  int comparacion = comparador(elemento, nodo_padre->elemento);
+
+    if(comparacion==0) return nodo_padre->elemento;
+    else if(comparacion<0){
+        if(!nodo_padre->izquierda) return NULL;
+        else return abb_buscar_recursivo(nodo_padre->izquierda, elemento, comparador);
+    }
+    else if(comparacion>0){
+        if(!nodo_padre->derecha) return NULL;
+        else return abb_buscar_recursivo(nodo_padre->derecha, elemento, comparador);
+    }
+
+    return NULL; 
+
+
 }
 
 void* abb_buscar(abb_t* arbol, void* elemento){
-  return NULL;
+  if(!arbol) return NULL;
+
+  int comparacion = comparador(elemento, arbol->nodo_raiz->elemento);
+
+  if(comparacion==0) return nodo_raiz->elemento;
+    else if(comparacion<0){
+        if(!nodo_raiz->izquierda) return NULL;
+        else return abb_buscar_recursivo(nodo_raiz->izquierda, elemento, arbol->comparador);
+    }
+    else if(comparacion>0){
+        if(!nodo_raiz->derecha) return NULL;
+        else return abb_buscar_recursivo(nodo_raiz->derecha, elemento, arbol->comparador);
+    }
+
+    return NULL;
 }
 
 bool abb_vacio(abb_t* arbol){
-  return true;
+  return (!arbol || arbol->tamanio==0);
 }
 
 size_t abb_tamanio(abb_t *arbol){
-  return 0;
+  if(!arbol) return 0;
+  return arbol->tamanio;
 }
 
 void abb_destruir(abb_t *arbol){
-
+  if(!arbol) return;
+  while(!abb_vacio(arbol)) abb_quitar(arbol, arbol->nodo_raiz->elemento);
+  free(arbol);
 }
 
 void abb_destruir_todo(abb_t *arbol, void (*destructor)(void *)){
+    if(!arbol || !destructor) return;
 
+    void* elemento_eliminar = NULL;
+
+    while(!abb_vacio(arbol)){
+        elemento_eliminar = abb_quitar(arbol, arbol->nodo_raiz->elemento);
+        destructor(elemento_eliminar);
+    }
+    free(arbol);
 }
 
 size_t abb_con_cada_elemento(abb_t *arbol, abb_recorrido recorrido, bool (*funcion)(void *, void *), void *aux){
